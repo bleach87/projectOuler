27c1fa36fe34b39267d9ce358fc0057d3fc94524
k.park <kinam.park28@gmail.com>
2016-03-14 18:29:17 +0900

    k.park prob_5

---
+++ projectOuler/src/projectOuler/Problem_5.java
@@ -0,0 +1,43 @@
+package projectOuler;
+
+import java.util.ArrayList;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Problem_5 {
+
+	public static void main(String[] args) {
+//		1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
+//		그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
+		
+		System.out.println(":::::problem_5:::::");
+		
+		int result = 11;
+//		ArrayList<String> resultArr = new ArrayList<String>();
+		ArrayList arrNum = new ArrayList();
+		
+		
+		for(int i=1; i<21;i++) {
+			arrNum.add(i);
+		}
+		
+		while(result>0){
+			boolean isCheck = true;
+			for(int i=0; i<arrNum.size(); i++){
+				if(result%(int)arrNum.get(i) != 0){
+					isCheck = false;
+					continue;
+				}
+			}
+			
+			if(isCheck){
+				System.out.println("result : "+ result );
+//				result = -1;
+				break;
+			}
+			result++;
+		}
+		
+	}
+
+}
